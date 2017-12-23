5befed17b609f17b4f83dde21cba22eb53f46155
3mb2d <tallwoodysw@gmail.com>
2017-12-23 20:02:15 +0000

    Completed problem 14 using a dictionary

--- Problem14/problem14.py
+++ Problem14/problem14.py
@@ -1,10 +1,32 @@
+import time
+import operator
+start = time.time()
 def collatz(n):
     if n % 2 == 0:
         return n/2
     else:
         return (3*n) + 1
+def keywithmaxval(d):
+    """ a) create a list of the dict's keys and values; 
+        b) return the key with the max value"""  
+    v=list(d.values())
+    k=list(d.keys())
+    return k[v.index(max(v))]
 longest_chain = [0,0]
 dictionary = {}
-for i in range(1000000):
+for i in range(2,1000001):
+    number = i
+    incomplete = True
     chain = 0
-print(longest_chain)
+    while incomplete:
+        if number in dictionary.keys():
+            incomplete = False
+            chain += dictionary[number]
+        else:
+            number = int(collatz(number))
+            chain += 1
+        if number == 1:
+            incomplete = False
+    dictionary[i] = chain
+print(keywithmaxval(dictionary))
+print(time.time()-start)
